133881c876a112d1b2f7effcdd0d513b2a3ad1d1
David Jenkins <david@theoremreach.com>
2021-08-06 10:37:33 -0400

    More safety

--- bin/update_latest.sh
+++ bin/update_latest.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-unitypackage="$1"
-
 if [ ! -d "./.git" ]
 then
         echo "This script must be ran from the root of the example app."
@@ -9,13 +7,14 @@ then
 fi
 
 initbranch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-if [ "${initbranch}" != "enhance/add_support_for_upm" ]
+if [ ${initbranch} != "enhance/add_support_for_upm" ]
 then
         echo "This script may only be ran from the master branch."
         exit 1
 fi
 
-if [ -f "${unitypackage}" ]
+unitypackage="$1"
+if [ -f ${unitypackage} ]
 then
         unity_package_name=(basename ${unitypackage})
         tmp_dir_name="uw-unity-$(date '+%Y-%m-%d')"
@@ -28,17 +27,22 @@ then
 
         ## switch to our latest branch
         git checkout latest
-        echo "Updated branch to 'latest'."
+        if [ $(git branch | sed -n -e 's/^\* \(.*\)/\1/p') != 'latest' ]
+        then
+                echo "Failed to update to 'latest' git branch. Exiting..."
+                exit 1
+        fi
 
         ## save our static README
         cp "./README.md" ${tmp_dir}
 
         ## remove files (DANGEROUS!)
-        read "You are about to remove all files in the 'latest' branch. Confirm? (y/n) " -n 1 -r
+        read -p "You are about to remove all files in the 'latest' branch. Confirm? (y/n) " -n 1 -r
         if [[ $REPLY = ^[Yy]$ ]]
         then
                 rm -rf ./*
         else
+                echo
                 echo "You must remove all files from 'latest' in order to generate a new version. Exiting..."
                 git checkout ${initbranch}
                 exit 1
