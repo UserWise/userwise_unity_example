04a9b1cc1913af074e3512d5fb67b613c0a8fcb5
David Jenkins <djenkinsdev@gmail.com>
2022-03-03 17:07:08 -0500

    Add runtime

---
+++ Runtime/Plugins/Plugins/Android/Utility.java
@@ -0,0 +1,15 @@
+package io.userwise.userwise_sdk;
+
+import com.unity3d.player.UnityPlayer;
+
+class UserWiseUtility {
+
+    public static String getCountry() {
+        return UnityPlayer.currentActivity.getResources().getConfiguration().locale.getCountry();
+    }
+    
+    public static String getLanguage() {
+        return UnityPlayer.currentActivity.getResources().getConfiguration().locale.getLanguage();
+    }
+}
+
